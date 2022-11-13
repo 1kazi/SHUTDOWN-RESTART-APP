bc03910ca49653d85bcd2f4f07cff251f388768c
kazi shakerodin <shakerodink@gmail.com>
2022-11-13 20:14:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import os
+
+
+def restart():
+    os.system("shutdown /r /t 1")
+
+
+def restart_time():
+    os.system("shutdown /r /t 20")
+
+
+def logout():
+    os.system("shutdown -l")
+
+
+def shutdown():
+    os.system("shutdown /s /t 1")
+
+
+st = Tk()
+st.title("shutDown App")
+st.geometry("800x800")
+st.config(bg="yellow")
+r_button = Button(st, text="Restart", font=(
+    "TIme New Roman", 30, "bold"), relief=RAISED, cursor="plus", command=restart)
+r_button.place(x=150, y=60, height=50, width=200)
+
+rt_button = Button(st, text="Restart Time", font=(
+    "TIme New Roman", 20, "bold"), relief=RAISED, cursor="plus", command=restart_time)
+rt_button.place(x=150, y=170, height=50, width=200)
+
+lg_button = Button(st, text="Log-Out", font=("TIme New Roman",
+                   30, "bold"), relief=RAISED, cursor="plus", command=logout)
+lg_button.place(x=150, y=260, height=50, width=200)
+
+st_button = Button(st, text="shutdown", font=(
+    "TIme New Roman", 30, "bold"), relief=RAISED, cursor="plus", command=shutdown)
+st_button.place(x=150, y=370, height=50, width=200)
+
+
+st.mainloop()
